a91198a777e0a0a113ed84bda900658f8e752ed6
Dmitriy Lobastov <lobastovdmitri@gmail.com>
2025-05-14 10:56:13 +0300

    New class Tasks. Moving logic from Main to Tasks

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -4,125 +4,48 @@ public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        // Практика Java. Классы и методы. #1
-        System.out.println("Получение дробной части числа.");
-        System.out.println("Введите вещественное число:");
-        double scannerDouble = scanner.nextDouble();
+        // Практика Java. Классы и методы.
+        System.out.println("Практика Java. Классы и методы.");
+        System.out.println("Задача 1. Получение дробной части числа.");
+        System.out.println("Задача 2. Сложение двух последних знаков.");
+        System.out.println("Задача 3. Вывод чисел от 0 до 9.");
+        System.out.println("Задача 4. Проверка, пложительное ли число.");
+        System.out.println("Задача 5. Проверка, двузначное ли число.");
+        System.out.println("Задача 6. Проверка, большая ли буква в диапазоне от 'A' до 'Z'.");
+        System.out.println("Задача 7. Проверка, входит ли num в диапазон числового ряда [a и b] включая границы.");
+        System.out.println("Задача 8. Проверка, делит ли любое число на другое нацело.");
+        System.out.println();
+        System.out.println("Для запуска задачи ведите её номер: ");
 
-        System.out.print("Дробная часть числа, 3 знака после запятой: ");
-        System.out.printf("%.3f%n%n", Logics.fraction(scannerDouble));
-
-        // Практика Java. Классы и методы. #2
-        System.out.println("Сложение двух последних знаков.");
-        System.out.println("Введите целое число не менее 2х знаков:");
         int scannerInt = scanner.nextInt();
 
-        if (scannerInt/10 !=0) {
-            System.out.println("Сумма двух последних чисел: " + Logics.sumLastNums(scannerInt));
-        }
-        else System.out.println("Вы ввели число менее 2х знаков!");
-        System.out.println();
-
-        // Практика Java. Классы и методы. #3
-        System.out.println("Вывод чисел от 0 до 9.");
-        System.out.println("Введите число от 0 до 9:");
-        String scannerString = scanner.next();
-        int resultCharToNum = Logics.charToNum(scannerString.charAt(0));
-        if (scannerString.length() == 1 && (resultCharToNum >=0 && resultCharToNum <=9)) {
-            System.out.println("Вы ввели число: " + resultCharToNum);
-        }
-        else {
-            System.out.println("Вы ввели не число от 0 до 9.");
-        }
-        System.out.println();
-
-        // Практика Java. Классы и методы. #4
-        System.out.println("Проверка, пложительное ли число.");
-        System.out.println("Введите целое число:");
-        scannerInt = scanner.nextInt();
-        if (Logics.isPositive(scannerInt)) {
-            System.out.println("Число положительное");
-        }
-        else {
-            System.out.println("Число отрицательное или 0");
-        }
-        System.out.println();
-
-        // Практика Java. Классы и методы. #5
-        System.out.println("Проверка, двузначное ли число.");
-        System.out.println("Введите положительное число:");
-        scannerInt = scanner.nextInt();
-
-        if (Logics.is2Digits(scannerInt)) {
-            System.out.println("Число двузначное");
-        }
-        else {
-            System.out.println("Число не является положительным и двузначным");
-        }
-
-        // Практика Java. Классы и методы. #6
-        System.out.println("Проверка, большая ли буква в диапазоне от 'A' до 'Z'");
-        System.out.println("Введите один символ");
-        scannerString = scanner.next();
-        boolean resultIsUpperCase = Logics.isUpperCase(scannerString.charAt(0));
-        if (scannerString.length() == 1 && resultIsUpperCase) {
-            System.out.println("Вы ввели большую ли букву в диапазоне от 'A' до 'Z'");
-        }
-        else {
-            System.out.println("Введеный текст не является большой буквой в диапазоне от 'A' до 'Z'");
-        }
-        System.out.println();
-
-        // Практика Java. Классы и методы. #7
-        System.out.println("Проверка, входит ли num в диапазон числового ряда [a и b] включая границы.");
-        System.out.println("Введите первое целое число a:");
-        int scannerIntA = scanner.nextInt();
-        System.out.println("Введите второе целое число b:");
-        int scannerIntB = scanner.nextInt();
-        System.out.println("Введите целое число num:");
-        int scannerIntNum = scanner.nextInt();
-
-        if (Logics.isInRange(scannerIntA, scannerIntB, scannerIntNum)) {
-            System.out.println("Num входит в диапазон числового ряда [a и b]");
-        }
-        else {
-            System.out.println("Num в диапазон числового ряда [a и b] не входит");
-        }
-
-        System.out.println();
-
-        // Практика Java. Классы и методы. #7
-        System.out.println("Проверка, делит ли любое число на другое нацело");
-        System.out.println("Введите одно целое число a:");
-        scannerIntA = scanner.nextInt();
-        System.out.println("Введите другое целое число b:");
-        scannerIntB = scanner.nextInt();
-
-        if (Logics.isDivisor(scannerIntA, scannerIntB) && scannerIntA !=0 && scannerIntB !=0) {
-            System.out.println("Одно любое число делится на другое без остатка");
-        }
-        else {
-            System.out.println("Одно любое число на другое без остатка не делится, либо одно или оба числа это ноль ");
-        }
-
-        System.out.println();
-
-
-        /*
-        System.out.println("Введите первое число:");
-        int firstNumber = scanner.nextInt();
-        System.out.println("Введите второе число:");
-        int secondNumber = scanner.nextInt();
-
-        int sum = firstNumber+secondNumber;
-        int diff = firstNumber-secondNumber;
-        int prod = firstNumber*secondNumber;
-        double priv = (double)firstNumber/secondNumber;
-
-        System.out.println("Сумма: " + sum);
-        System.out.println("Разность: " + diff);
-        System.out.println("Произведение: " + prod);
-        System.out.println("Частное: " + priv);
-         */
+        switch (scannerInt) {
+                case 1:
+                    Tasks.task1();
+                    break;
+                case 2:
+                    Tasks.task2();
+                    break;
+                case 3:
+                    Tasks.task3();
+                    break;
+                case 4:
+                    Tasks.task4();
+                    break;
+                case 5:
+                    Tasks.task5();
+                    break;
+                case 6:
+                    Tasks.task6();
+                    break;
+                case 7:
+                    Tasks.task7();
+                    break;
+                case 8:
+                    Tasks.task8();
+                    break;
+                default:
+                    System.out.println("Задача с таким номером отсутствует");
+            }
     }
 }
