4d71f7234746b3b398f961c3b994d42830ad0753
Dmitriy Lobastov <lobastovdmitri@gmail.com>
2025-05-19 00:16:00 +0300

    refactoring Main

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,23 +2,16 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        Scanner number = new Scanner(System.in);
-        int firstNumber;
-        int secondNumber;
-        int sum;
-        int diff;
-        int prod;
-        double priv;
-
+        Scanner scanner = new Scanner(System.in);
         System.out.println("Введите первое число:");
-        firstNumber = number.nextInt();
+        int firstNumber = scanner.nextInt();
         System.out.println("Введите второе число:");
-        secondNumber = number.nextInt();
+        int secondNumber = scanner.nextInt();
 
-        sum = firstNumber+secondNumber;
-        diff = firstNumber-secondNumber;
-        prod = firstNumber*secondNumber;
-        priv = (double)firstNumber/secondNumber;
+        int sum = firstNumber+secondNumber;
+        int diff = firstNumber-secondNumber;
+        int  prod = firstNumber*secondNumber;
+        double  priv = (double) firstNumber/secondNumber;
 
         System.out.println("Сумма: " + sum);
         System.out.println("Разность: " + diff);
